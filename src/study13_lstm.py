40e637c801fa1283b6d82d07dcf1ffd3f8d5d7f5
yuanmc <438353531@qq.com>
2018-11-16 14:50:41 +0800

    添加study13：lstm

---
+++ src/study13_lstm.py
@@ -0,0 +1,108 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+lr = 0.001
+traning_iters = 100000
+batch_size = 128
+
+n_inputs = 28
+n_steps = 28
+n_hidden_units = 128
+n_class = 10
+
+x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.float32, [None, n_class])
+
+weights = {
+    "in": tf.Variable(tf.random_normal([n_inputs, n_hidden_units])),
+    "out": tf.Variable(tf.random_normal([n_hidden_units, n_class]))
+}
+
+biases = {
+    "in": tf.Variable(tf.constant(0.1, shape=[n_hidden_units, ])),
+    "out": tf.Variable(tf.constant(0.1, shape=[n_class, ]))
+}
+
+
+def RNN(X, weights, biases):
+    # hidden layer for input to cell
+    ########################################
+
+    # transpose the inputs shape from
+    # X ==> (128 batch * 28 steps, 28 inputs)
+    X = tf.reshape(X, [-1, n_inputs])
+
+    # into hidden
+    # X_in = (128 batch * 28 steps, 128 hidden)
+    X_in = tf.matmul(X, weights['in']) + biases['in']
+    # X_in ==> (128 batch, 28 steps, 128 hidden)
+    X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])
+
+    # cell
+    ##########################################
+
+    # basic LSTM Cell.
+    if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+        cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
+    else:
+        cell = tf.contrib.rnn.BasicLSTMCell(n_hidden_units)
+    # lstm cell is divided into two parts (c_state, h_state)
+    init_state = cell.zero_state(batch_size, dtype=tf.float32)
+
+    # You have 2 options for following step.
+    # 1: tf.nn.rnn(cell, inputs);
+    # 2: tf.nn.dynamic_rnn(cell, inputs).
+    # If use option 1, you have to modified the shape of X_in, go and check out this:
+    # https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/recurrent_network.py
+    # In here, we go for option 2.
+    # dynamic_rnn receive Tensor (batch, steps, inputs) or (steps, batch, inputs) as X_in.
+    # Make sure the time_major is changed accordingly.
+    # time_major=False的意思和pytorch中batch_first = true是一样的，意思是step在batch维度的后面
+    outputs, final_state = tf.nn.dynamic_rnn(cell, X_in, initial_state=init_state, time_major=False)
+
+    # hidden layer for output as the final results
+    #############################################
+    # results = tf.matmul(final_state[1], weights['out']) + biases['out']
+
+    # # or
+    # unpack to list [(batch, outputs)..] * steps
+    if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+        outputs = tf.unpack(tf.transpose(outputs, [1, 0, 2]))    # states is the last outputs
+    else:
+        outputs = tf.unstack(tf.transpose(outputs, [1,0,2]))
+    results = tf.matmul(outputs[-1], weights['out']) + biases['out']    # shape = (128, 10)
+
+    return results
+
+
+pred = RNN(x, weights, biases)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+train_op = tf.train.AdamOptimizer(lr).minimize(cost)
+
+correct_pred = tf.equal(tf.argmax(pred, axis=1), tf.argmax(y, axis=1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    step = 0
+    while step * batch_size < traning_iters:
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
+        sess.run([train_op], feed_dict={
+            x: batch_xs,
+            y: batch_ys,
+        })
+        if step % 20 == 0:
+            test_x, test_y = mnist.test.next_batch(batch_size)
+            test_x = test_x.reshape([batch_size, n_steps, n_inputs])
+            print("train:", sess.run(accuracy, feed_dict={
+                x:  batch_xs,
+                y:  batch_ys,
+            }), "test:", sess.run(accuracy, feed_dict={
+                x:  test_x,
+                y:  test_y,
+            }))
--- src/study9_classifiaction.py
+++ src/study9_classifiaction.py
@@ -2,52 +2,51 @@ import tensorflow as tf
 import numpy as np
 import tensorflow.examples.tutorials.mnist.input_data as input_data
 
-
-def add_layer(inputs, in_size, out_size, activation_function=None):
-    with tf.name_scope('layer'):
-        with tf.name_scope('weight'):
-            # 用tf.random_normal生成随机变量作为初始值，比全为0要好很多
-            weights = tf.Variable(tf.random_normal([in_size, out_size]))
-        with tf.name_scope('biases'):
-            # 推荐初始值不为0，所有初始为0.1
-            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-        with tf.name_scope('Wx_plus_b'):
-            Wx_plus_b = tf.matmul(inputs, weights) + biases
-        if activation_function is None:
-            outputs = Wx_plus_b
-        else:
-            # 激活函数默认有名字，可以不写
-            outputs = activation_function(Wx_plus_b)
-        return outputs
-
-
-def compute_accuracy(v_xs, v_ys):
-    global preediction
-    y_pre = sess.run(prediction, feed_dict={xs: v_xs})
-    correct_prediction = tf.equal(tf.argmax(y_pre, 1), tf.argmax(v_ys, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
-    return result
+with tf.device('/gpu:0'):
+    def add_layer(inputs, in_size, out_size, activation_function=None):
+        with tf.name_scope('layer'):
+            with tf.name_scope('weight'):
+                # 用tf.random_normal生成随机变量作为初始值，比全为0要好很多
+                weights = tf.Variable(tf.random_normal([in_size, out_size]))
+            with tf.name_scope('biases'):
+                # 推荐初始值不为0，所有初始为0.1
+                biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+            with tf.name_scope('Wx_plus_b'):
+                Wx_plus_b = tf.matmul(inputs, weights) + biases
+            if activation_function is None:
+                outputs = Wx_plus_b
+            else:
+                # 激活函数默认有名字，可以不写
+                outputs = activation_function(Wx_plus_b)
+            return outputs
 
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-xs = tf.placeholder(tf.float32, [None, 784])  # 28*28
-ys = tf.placeholder(tf.float32, [None, 10])
+    def compute_accuracy(v_xs, v_ys):
+        global preediction
+        y_pre = sess.run(prediction, feed_dict={xs: v_xs})
+        correct_prediction = tf.equal(tf.argmax(y_pre, 1), tf.argmax(v_ys, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
+        return result
 
-prediction = add_layer(xs, 784, 10, activation_function=tf.nn.softmax)
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    xs = tf.placeholder(tf.float32, [None, 784])  # 28*28
+    ys = tf.placeholder(tf.float32, [None, 10])
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction), axis=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.4).minimize(cross_entropy)
-max_accuracy = 0
-with tf.Session() as sess:
-    sess.run(tf.initialize_all_variables())
-    for step in range(50000):
-        batch_xs, batch_ys = mnist.train.next_batch(100)
-        sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
-        # if step % 50 == 0:
-        loss = sess.run(cross_entropy, feed_dict={xs: batch_xs, ys: batch_ys})
-        accuracy = compute_accuracy(mnist.test.images, mnist.test.labels)
-        if accuracy > max_accuracy:
-            max_accuracy = accuracy
-            print('step:', step, 'loss:', loss, 'accuracy:', accuracy)
-    print("最高准确率", max_accuracy)
+    prediction = add_layer(xs, 784, 10, activation_function=tf.nn.softmax)
+
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction), axis=[1]))
+    train_step = tf.train.GradientDescentOptimizer(0.4).minimize(cross_entropy)
+    max_accuracy = 0
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+        for step in range(50000):
+            batch_xs, batch_ys = mnist.train.next_batch(100)
+            sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
+            # if step % 50 == 0:
+            loss = sess.run(cross_entropy, feed_dict={xs: batch_xs, ys: batch_ys})
+            accuracy = compute_accuracy(mnist.test.images, mnist.test.labels)
+            if accuracy > max_accuracy:
+                max_accuracy = accuracy
+                print('step:', step, 'loss:', loss, 'accuracy:', accuracy)
+        print("最高准确率", max_accuracy)
